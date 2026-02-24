c25c3263267c11c48141ee512c1775e0ca127fa7
jjahamez <stephenchenstephenchen1211@gmail.com>
2026-02-24 12:58:29 -0500

    Improved styling significantly

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const b: number = Number(formData.get("b"));
 const c: number = Number(formData.get("c"));
 const d: number = Number(formData.get("d"))
 
-const test = (a + b + c) * d ;
+const test = a + b + c + d ;
 (document.getElementById("result") as HTMLInputElement).value = test.toString();
 
 })

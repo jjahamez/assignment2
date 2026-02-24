43c8a13d6beea90ca7de244b9648c1f90883eea5
jjahamez <stephenchenstephenchen1211@gmail.com>
2026-02-23 22:41:00 -0500

    Added straw code for the form to work(check Mr. Q's github)

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+const form = document.getElementById("cubic-form") as HTMLFormElement;
+
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+
+const formData = new FormData(form); 
+
+const a: number = Number(formData.get("a"));
+const b: number = Number(formData.get("b"));
+const c: number = Number(formData.get("c"));
+const d: number = Number(formData.get("d"))
+
+const test = a + b + c + d ;
+(document.getElementById("result") as HTMLInputElement).value = test.toString();
+
+})

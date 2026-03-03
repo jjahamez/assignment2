a712e242456efe08db8e8aa18de409005cff329d
jjahamez <stephenchenstephenchen1211@gmail.com>
2026-03-03 01:59:34 -0500

    Added calculations for when discriminant (△) is <0

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,30 @@ form?.addEventListener("submit", (event) => {
 
   const p = (3 * a * c - b * b) / (3 * a * a); /* depressed cubic, refer to mr q */
   const q = (27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a * a);
-  const discriminant = -4 * Math.pow(p, 3) - 27 * Math.pow(q, 2);
+  const discriminant = Math.pow(q/2, 2) + Math.pow(p/3, 3);
 
-  if (discriminant > 0) {
-    (document.getElementById("result") as HTMLInputElement).value = "3 real roots";
-  } else if (discriminant < 0) {
+  const h = -b / (3 * a); /* to equate y and x, adjuster */
+
+  if (discriminant < 0) { 
+    const theta = Math.acos(((-q / (2 * Math.sqrt((-p/3)**3))))) / 3;
+    const k = 2 * Math.sqrt(-p/3); 
+
+    const y1 = k * Math.cos(theta);
+    const y2 = k * Math.cos(theta + 2 * Math.PI / 3); 
+    const y3 = k * Math.cos(theta + 4 * Math.PI / 3); 
+
+    const root1 = y1 + h;
+    const root2 = y2 + h;
+    const root3 = y3 + h; /* more efficient to just remove y? */
+    
+    (document.getElementById("result") as HTMLInputElement).value = `x1=${root1.toFixed(4)}, x2=${root2.toFixed(4)}, x3=${root3.toFixed(4)}`; 
+    /* trig method */ 
+    
+  } else if (discriminant > 0) {
     (document.getElementById("result") as HTMLInputElement).value = "1 real root, 2 complex";
-  } else {
+  
+  } else {  
     (document.getElementById("result") as HTMLInputElement).value = "Repeated roots (Triple or Double/Single";
   }
 
-})
+})  

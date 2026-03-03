4fae920a060eb2b851d46453a9524436deda5419
jjahamez <stephenchenstephenchen1211@gmail.com>
2026-03-03 01:18:05 -0500

    Calculated discriminant and cases added (without calculation)

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ form?.addEventListener("submit", (event) => {
   event.preventDefault();
 
 
-const formData = new FormData(form); 
+  const formData = new FormData(form);
 
-const a: number = Number(formData.get("a"));
-const b: number = Number(formData.get("b"));
-const c: number = Number(formData.get("c"));
-const d: number = Number(formData.get("d"))
+  const a: number = Number(formData.get("a"));
+  const b: number = Number(formData.get("b"));
+  const c: number = Number(formData.get("c"));
+  const d: number = Number(formData.get("d"));
 
-const test = a + b + c + d ;
-(document.getElementById("result") as HTMLInputElement).value = test.toString();
+  const p = (3 * a * c - b * b) / (3 * a * a); /* depressed cubic, refer to mr q */
+  const q = (27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a * a);
+  const discriminant = -4 * Math.pow(p, 3) - 27 * Math.pow(q, 2);
+
+  if (discriminant > 0) {
+    (document.getElementById("result") as HTMLInputElement).value = "3 real roots";
+  } else if (discriminant < 0) {
+    (document.getElementById("result") as HTMLInputElement).value = "1 real root, 2 complex";
+  } else {
+    (document.getElementById("result") as HTMLInputElement).value = "Repeated roots (Triple or Double/Single";
+  }
 
 })
